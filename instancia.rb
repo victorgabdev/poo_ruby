a6ff2daa1977724ffb1e43f3b0c09701698e485b
Victor Gabriel <victorgabdev@gmail.com>
2023-09-28 23:47:32 -0300

    arquivo 'instancia' criado

---
+++ instancia.rb
@@ -0,0 +1,14 @@
+class User
+    def add(name)
+        @name = name # Variável de instância
+        puts "User adicionado"
+        hello
+    end
+
+    def hello
+        puts "Seja bem vindo, #{@name}"
+    end
+end
+
+user = User.new
+user.add("victor")
